ca3a1d0c43ff48df1780b76db420f8504ef73774
Hyeonjun Sin <40359803+sinaenjuni@users.noreply.github.com>
2020-01-15 12:17:16 +0900

    Inital project

---
+++ app/src/main/java/y2k4388/gmail/com/pickoneforme/MainActivity.java
@@ -0,0 +1,14 @@
+package y2k4388.gmail.com.pickoneforme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
